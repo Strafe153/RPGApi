55e7bc564ce68a7ce903e181b6f09e36a998a198
Vitalik Knysh <knish.vital@gmail.com>
2022-01-02 19:21:59 +0200

    Add Player model

--- RPGApi/Program.cs
+++ RPGApi/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RPGApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("defaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
