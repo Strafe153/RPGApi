c1c9246ea1430008a9754da61fa017e84db9d9e1
Vitalik Knysh <knish.vital@gmail.com>
2022-01-22 03:33:18 +0200

    Create PlayersController ChangeRole POST request

---
+++ RPGApi/Dtos/Players/PlayerChangeRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RPGApi.Data;
+
+namespace RPGApi.Dtos.Players
+{
+    public record PlayerChangeRoleDto
+    {
+        [Required]
+        public Guid Id { get; init; }
+
+        public PlayerRole Role { get; init; } = PlayerRole.Player;
+    }
+}
