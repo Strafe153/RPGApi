f927b41be0273650a9ba061e3a7bee65eba73395
Vitalik Knysh <knish.vital@gmail.com>
2023-07-19 04:54:23 +0300

    Made minor styling changes in the controllers configuration

--- WebApi/Configurations/ControllersConfiguration.cs
+++ WebApi/Configurations/ControllersConfiguration.cs
@@ -8,15 +8,16 @@ public static class ControllersConfiguration
 {
     public static void ConfigureControllers(this IServiceCollection services)
     {
-        services.AddControllers(options =>
-        {
-            options.SuppressAsyncSuffixInActionNames = false;
-            options.Filters.Add<ExceptionHandlingFilter>();
-        })
-        .AddNewtonsoftJson(options =>
-        {
-            options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-        });
+        services
+            .AddControllers(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+                options.Filters.Add<ExceptionHandlingFilter>();
+            })
+            .AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
     }
 }
