5b3cf9ff53f8cba7560a93882f29020987486174
Vitalik Knysh <knish.vital@gmail.com>
2024-04-13 12:54:29 +0300

    Slightly edited the exception handling filter

--- WebApi/Filters/ExceptionHandlingFilterAttribute.cs
+++ WebApi/Filters/ExceptionHandlingFilterAttribute.cs
@@ -4,60 +4,61 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Net;
+using System.Net.Mime;
 
 namespace WebApi.Filters;
 
 public class ExceptionHandlingFilterAttribute : ExceptionFilterAttribute
 {
-    public override void OnException(ExceptionContext context)
-    {
-        var statusCode = GetHttpStatusCode(context.Exception);
-        int statusCodeAsInt = (int)statusCode;
+	public override void OnException(ExceptionContext context)
+	{
+		var statusCode = GetHttpStatusCode(context.Exception);
+		int statusCodeAsInt = (int)statusCode;
 
-        context.HttpContext.Response.ContentType = "application/json";
-        context.HttpContext.Response.StatusCode = statusCodeAsInt;
+		context.HttpContext.Response.ContentType = MediaTypeNames.Application.Json;
+		context.HttpContext.Response.StatusCode = statusCodeAsInt;
 
-        var problemDetails = new ProblemDetails
-        {
-            Type = GetRFCType(statusCode),
-            Title = context.Exception.Message,
-            Status = statusCodeAsInt,
-            Detail = context.Exception.Message,
-            Instance = context.HttpContext.Request.Path
-        };
+		ProblemDetails problemDetails = new()
+		{
+			Type = GetRFCType(statusCode),
+			Title = context.Exception.Message,
+			Status = statusCodeAsInt,
+			Detail = context.Exception.Message,
+			Instance = context.HttpContext.Request.Path
+		};
 
-        context.Result = new JsonResult(problemDetails)
-        {
-            StatusCode = statusCodeAsInt
-        };
+		context.Result = new JsonResult(problemDetails)
+		{
+			StatusCode = statusCodeAsInt
+		};
 
-        base.OnException(context);
-    }
+		base.OnException(context);
+	}
 
-    private static HttpStatusCode GetHttpStatusCode(Exception exception)
-    {
-        return exception switch
-        {
-            NullReferenceException
-                or ItemNotFoundException => HttpStatusCode.NotFound,
-            NotEnoughRightsException
-                or InvalidTokenException => HttpStatusCode.Forbidden,
-            IncorrectPasswordException
-                or NameNotUniqueException
-                or OperationCanceledException
-                or PostgresException => HttpStatusCode.BadRequest,
-            _ => HttpStatusCode.InternalServerError
-        };
-    }
+	private static HttpStatusCode GetHttpStatusCode(Exception exception)
+	{
+		return exception switch
+		{
+			NullReferenceException
+				or ItemNotFoundException => HttpStatusCode.NotFound,
+			NotEnoughRightsException
+				or InvalidTokenException => HttpStatusCode.Forbidden,
+			IncorrectPasswordException
+				or NameNotUniqueException
+				or OperationCanceledException
+				or PostgresException => HttpStatusCode.BadRequest,
+			_ => HttpStatusCode.InternalServerError
+		};
+	}
 
-    private static string GetRFCType(HttpStatusCode statusCode)
-    {
-        return statusCode switch
-        {
-            HttpStatusCode.BadRequest => RFCType.BadRequest,
-            HttpStatusCode.Forbidden => RFCType.Forbidden,
-            HttpStatusCode.NotFound => RFCType.NotFound,
-            _ => RFCType.InternalServerError
-        };
-    }
+	private static string GetRFCType(HttpStatusCode statusCode)
+	{
+		return statusCode switch
+		{
+			HttpStatusCode.BadRequest => RFCType.BadRequest,
+			HttpStatusCode.Forbidden => RFCType.Forbidden,
+			HttpStatusCode.NotFound => RFCType.NotFound,
+			_ => RFCType.InternalServerError
+		};
+	}
 }
